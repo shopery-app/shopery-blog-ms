bc631af79a893e3a49354e514e0f0212ff580a6b
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-25 13:44:55 +0400

    optimization: remove unnecessary files and codes

--- src/main/java/az/shopery/blog_ms/repository/UserRepository.java
+++ src/main/java/az/shopery/blog_ms/repository/UserRepository.java
@@ -1,30 +1,13 @@
 package az.shopery.blog_ms.repository;
 
 import az.shopery.blog_ms.model.entity.UserEntity;
-import az.shopery.blog_ms.util.enums.SubscriptionTier;
-import az.shopery.blog_ms.util.enums.UserRole;
 import az.shopery.blog_ms.util.enums.UserStatus;
-import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmailAndStatus(String email, UserStatus status);
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT u FROM UserEntity u WHERE u.email = :email")
-    Optional<UserEntity> findAndLockByEmail(String email);
-    Boolean existsByEmail(String email);
-    Optional<UserEntity> findByEmailAndUserRoleAndStatus(String email, UserRole userRole, UserStatus status);
-    Page<UserEntity> findAllByUserRoleAndStatus(UserRole userRole, UserStatus status, Pageable pageable);
-    Optional<UserEntity> findByEmailAndUserRoleAndStatusAndSubscriptionTier(String email, UserRole userRole, UserStatus status, SubscriptionTier subscriptionTier);
-
-    @Query(value = "SELECT * FROM users WHERE user_role = 'ADMIN' AND status = 'ACTIVE' ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
-    Optional<UserEntity> findRandomActiveAdmin();
 }
