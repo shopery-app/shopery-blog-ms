12566c011a8d6f0a00a72c33609591bc3771fbce
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-25 12:38:40 +0400

    project setup

---
+++ src/main/java/az/shopery/blog_ms/BlogMsApplication.java
@@ -0,0 +1,13 @@
+package az.shopery.blog_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/shopery/blog_ms/BlogMsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.shopery.blog_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
