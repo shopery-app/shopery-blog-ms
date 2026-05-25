262c69189e698e223d218e3cbea18b2ab4165da4
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-25 12:46:28 +0400

    config: handle database connection

--- src/main/java/az/shopery/blog_ms/BlogMsApplication.java
+++ src/main/java/az/shopery/blog_ms/BlogMsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogMsApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(BlogMsApplication.class, args);
 	}
-
 }
